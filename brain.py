94a84154fd0e6cf5f9787c0a45998a36ca822e19
SeHan Choi <shchoi@DI-SOLUTION.CO.KR>
2022-09-20 16:06:15 +0900

    test

--- brain.py
+++ brain.py
@@ -2,37 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from model import *
+
 class CFG:
     lr = 3e-4
     loss_threshold = 0.001
     
-class NeuralNet(nn.Module):
-
-    def __init__(self):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(100, 512)
-        self.layer1 = self.make_layers(512, num_repeat=10)
-        self.fc5 = nn.Linear(512, 100)
-
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.layer1(x)
-        # x = nn.Dropout(0.2)(x)
-        x = self.fc5(x)
-        return x
-
-    def make_layers(self, value, num_repeat):
-        layers = []
-        for _ in range(num_repeat):
-            layers.append(nn.Linear(value, value))
-            layers.append(nn.ReLU(inplace=True))
-            layers.append(nn.Dropout(0.2))
-
-        return nn.Sequential(*layers)
-
 # loss값 이상하게 나올수도있으니 마지막으로 체크할것.
 
 if __name__ == "__main__":
